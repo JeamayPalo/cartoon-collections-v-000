e5c15b6cf2d0f4405199ca8f91878ab45be6b93f
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-11 20:29:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,9 @@
 
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.each_with_index {|name, index| array[name] = index}
+  array.each_with_index do |name, index|
+    array[name] = index
+  end
 end
 
 def summon_captain_planet# code an argument here
