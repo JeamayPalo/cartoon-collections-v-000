3c48ad83f5151d97bdcc6e705c62a43e0d13cebf
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-11 21:03:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,11 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.collect {|x| x.length > 2}  ?  true : false
+  array.collect { if |x| x.length > 2} 
+   return true 
+  {elsif |x| x.length < 4} 
+    return false
+  else nil
 end
 
 def find_the_cheese# code an argument here
