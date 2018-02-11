135504e0725b1a05ba5ad0c3dd94d644b2c68da9
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-11 20:58:26 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,9 +11,8 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  array.collect {|x| x.length > 2 ?  true : false}
-
   # Your code here
+  array.collect {|x| x.length > 2 ?  true : false}
 end
 
 def find_the_cheese# code an argument here
