c80ab73fdf2407af6ac0e590b639d532c837d6eb
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-11 21:35:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def find_the_cheese(array)# code an argument here
   cheese_types.collect do |cheese|
       if array.include?(cheese)
     return cheese
-    break
+    else break
     end
   end
 end
