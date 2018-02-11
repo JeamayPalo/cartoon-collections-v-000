7ac7f00a387b012d8d767cc5e77972cbdbce48f4
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-11 21:05:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.any? {|x| x.length > 4} 
+  array.any? {|x| x.length > 4}
 end
 
 def find_the_cheese# code an argument here
